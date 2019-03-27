afa95b8dfcf4ff0c3374c1b67a5f9af37b31f689
davemarch <davemarchaudio@gmail.com>
2019-03-27 21:08:23 +0000

    Updates

---
+++ precache-manifest.fcf9977467ee9276fca2ba97ca2c229b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb42db7331e6d7b47ff9f8ef4409dc7",
+    "url": "/stubucks/static/media/buyCoffee.5fb42db7.jpg"
+  },
+  {
+    "revision": "5b47116d27beed9a44b9",
+    "url": "/stubucks/static/css/main.5d484882.chunk.css"
+  },
+  {
+    "revision": "e84069ab57254b164cda",
+    "url": "/stubucks/static/js/runtime~main.2c0cf6f9.js"
+  },
+  {
+    "revision": "fce4675a91282d575a27",
+    "url": "/stubucks/static/js/2.7c2f92a1.chunk.js"
+  },
+  {
+    "revision": "a539894fdfcf8109073f33b8e621db69",
+    "url": "/stubucks/static/media/yogamoon.a539894f.png"
+  },
+  {
+    "revision": "e9514e6da888d93dab249bfa7460098a",
+    "url": "/stubucks/static/media/yogaphoto1.e9514e6d.jpeg"
+  },
+  {
+    "revision": "5b47116d27beed9a44b9",
+    "url": "/stubucks/static/js/main.a7a17e58.chunk.js"
+  },
+  {
+    "revision": "f872f55e4f418ab3c0e199376e62cd0a",
+    "url": "/stubucks/static/media/buyTea.f872f55e.jpg"
+  },
+  {
+    "revision": "80bb634e39454a7d66bb01421c0ce300",
+    "url": "/stubucks/static/media/buyEquipment.80bb634e.png"
+  },
+  {
+    "revision": "5e00badedb58df5b57325e03227c2e9b",
+    "url": "/stubucks/static/media/StubrewIcedCoffee.5e00bade.png"
+  },
+  {
+    "revision": "3cc2b029e09daec5595ef97195bd9b90",
+    "url": "/stubucks/static/media/map.3cc2b029.png"
+  },
+  {
+    "revision": "ccc606a59503ca4bbb311a3013686cce",
+    "url": "/stubucks/index.html"
+  }
+];
