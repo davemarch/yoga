3daa4255d6878404b26788fdc16a3e6b227862db
davemarch <davemarchaudio@gmail.com>
2019-03-27 21:09:12 +0000

    Updates

---
+++ precache-manifest.fb4c2df9c81a79237d24b2649cd7ac79.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb42db7331e6d7b47ff9f8ef4409dc7",
+    "url": "/yoga/static/media/buyCoffee.5fb42db7.jpg"
+  },
+  {
+    "revision": "cb584f33d9df35f30ed7",
+    "url": "/yoga/static/css/main.5d484882.chunk.css"
+  },
+  {
+    "revision": "32c3a96c9dbbd27a99d5",
+    "url": "/yoga/static/js/runtime~main.42c0f37f.js"
+  },
+  {
+    "revision": "fce4675a91282d575a27",
+    "url": "/yoga/static/js/2.7c2f92a1.chunk.js"
+  },
+  {
+    "revision": "a539894fdfcf8109073f33b8e621db69",
+    "url": "/yoga/static/media/yogamoon.a539894f.png"
+  },
+  {
+    "revision": "e9514e6da888d93dab249bfa7460098a",
+    "url": "/yoga/static/media/yogaphoto1.e9514e6d.jpeg"
+  },
+  {
+    "revision": "cb584f33d9df35f30ed7",
+    "url": "/yoga/static/js/main.c9d491c2.chunk.js"
+  },
+  {
+    "revision": "f872f55e4f418ab3c0e199376e62cd0a",
+    "url": "/yoga/static/media/buyTea.f872f55e.jpg"
+  },
+  {
+    "revision": "80bb634e39454a7d66bb01421c0ce300",
+    "url": "/yoga/static/media/buyEquipment.80bb634e.png"
+  },
+  {
+    "revision": "5e00badedb58df5b57325e03227c2e9b",
+    "url": "/yoga/static/media/StubrewIcedCoffee.5e00bade.png"
+  },
+  {
+    "revision": "3cc2b029e09daec5595ef97195bd9b90",
+    "url": "/yoga/static/media/map.3cc2b029.png"
+  },
+  {
+    "revision": "21dd8472df45c0790bea81b97fa6b299",
+    "url": "/yoga/index.html"
+  }
+];
